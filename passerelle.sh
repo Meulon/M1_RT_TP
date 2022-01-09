790a1bcae280e34e09d4b2d169a2f8070a253925
meulon <marlon@ik.me>
2022-01-09 11:07:22 +0100

    correction paquets

--- passerelle.sh
+++ passerelle.sh
@@ -4,4 +4,4 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
-sudo apt install -y docker-ce mosquito python-paho-mqtt
+sudo apt install -y docker-ce mosquitto python3-paho-mqtt 
