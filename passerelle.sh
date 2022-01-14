269e8e964865b26521b41dea027663355526f372
meulon <marlon@ik.me>
2022-01-14 05:31:40 +0100

    add lxc qemu paquets

--- passerelle.sh
+++ passerelle.sh
@@ -4,4 +4,4 @@ sudo apt install -y apt-transport-https ca-certificates curl software-properties
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
-sudo apt install -y docker-ce mosquitto python3-paho-mqtt 
+sudo apt install -y docker-ce mosquitto python3-paho-mqtt lxc qemu qemu-utils qemu-system-x86
