8f104899d6ee22363d6281b9791fe8b7d642d151
marlon <marlon@ik.me>
2022-01-23 17:43:09 +0100

    fix network docker

--- eventManager.sh
+++ eventManager.sh
@@ -2,6 +2,7 @@
 
 docker pull eclipse-mosquitto
 docker run -d --name eventmanager -p 1884:1883 -p 9002:9001 --net=host eclipse-mosquitto
+sudo ovs-docker add-port br eth1 eventmanager --ipaddress=192.168.1.2/24 --gateway=192.168.1.1
 
 # join docker to network
 # ovs-docker add-port br eth1 eventmanager --ipaddress=192.168.1.2/24 --gateway=192.168.1.1
--- webServer.sh
+++ webServer.sh
@@ -2,7 +2,8 @@
 
 docker build -t webserver webServer/
 docker run -d --name webserver -p 80:80 webserver
+sudo ovs-docker add-port br eth1 webserver --ipaddress=192.168.1.5/24 --gateway=192.168.1.1
 
 # how to join docker to network
-# ovs-docker add-port br eth1 webserver --ipaddress=192.168.1.5/24 --gateway=192.168.1.1
-# ovs-vsctl list-ports br
+# sudo ovs-docker add-port br eth1 webserver --ipaddress=192.168.1.5/24 --gateway=192.168.1.1
+# sudo ovs-vsctl list-ports br
