4318fc0d8e236d31262d834c68b10d4377d5eb94
marlon <marlon@ik.me>
2022-01-23 17:29:11 +0100

    eventManager fix network

--- eventManager.sh
+++ eventManager.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 docker pull eclipse-mosquitto
-docker run -d --name eventmanager -p 1883:1883 -p 9001:9001 --net=host eclipse-mosquitto
+docker run -d --name eventmanager -p 1884:1883 -p 9002:9001 --net=host eclipse-mosquitto
 
+# join docker to network
+# ovs-docker add-port br eth1 eventmanager --ipaddress=192.168.1.2/24 --gateway=192.168.1.1
 # test
 # mosquitto_sub -t house/# -h localhost
 # mosquitto_pub -h localhost -m "test message" -t house/bulb4
