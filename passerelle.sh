039ba02ce2553b9783628a9f7d20b381f4b84a41
marlon <marlon@ik.me>
2022-01-23 17:39:26 +0100

    fix sudo cmd

--- passerelle.sh
+++ passerelle.sh
@@ -8,17 +8,17 @@ sudo apt-get update
 sudo apt-get install -y docker-ce python3-pip lxc mosquitto qemu qemu-utils qemu-system-x86 openvswitch-common openvswitch-switch 
 pip install paho-mqtt
 
-cp ovs-ifup /etc/
-cp ovs-ifdown /etc/
+sudo cp ovs-ifup /etc/
+sudo cp ovs-ifdown /etc/
 
-chmod +x /etc/ovs-if{up,down}
+sudo chmod +x /etc/ovs-if{up,down}
 
-ovs-vsctl add-br br
-ip addr add 192.168.1.1/28 dev br
-ip link set dev br up
+sudo ovs-vsctl add-br br
+sudo ip addr add 192.168.1.1/28 dev br
+sudo ip link set dev br up
 
-ovs-vsctl add-br br-v
-ip addr add 10.0.0.0/8 dev br-v
-ip link set dev br-v up
+sudo ovs-vsctl add-br br-v
+sudo ip addr add 10.0.0.1/8 dev br-v
+sudo ip link set dev br-v up
 
-ip link set dev ovs-system up
+sudo ip link set dev ovs-system up
