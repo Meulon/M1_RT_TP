4c23dde02866ca7179e664fdd1f28944bfa04a0f
marlon <marlon@ik.me>
2022-01-23 16:22:44 +0100

    fix webserver network

--- webServer.sh
+++ webServer.sh
@@ -4,5 +4,5 @@ docker build -t webserver webServer/
 docker run -d --name webserver -p 80:80 webserver
 
 # how to join docker to network
-# ovs-docker add-port br eth0 alpine1 --ipaddress=192.168.1.10/24 --gateway=192.168.1.1
+# ovs-docker add-port br eth1 webserver --ipaddress=192.168.1.5/24 --gateway=192.168.1.1
 # ovs-vsctl list-ports br
