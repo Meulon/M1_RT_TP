f1f63bf255d49cb2e36ce6ce28181b26c9080f9a
marlon <marlon@ik.me>
2022-01-23 20:25:44 +0100

    fix

--- vehicule.sh
+++ vehicule.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 sudo lxc-create -n vehicule -f vehicule/config -t download -- -d alpine -r 3.15 -a amd64
-
-sudo lxc-start -n vehicule
-
+sudo lxc-start -n vehicule -f vehicule/config
 sudo ovs-vsctl add-port br-v br-clone
 
 lxc-attach -n vehicule
 adduser driver
-
 apk add python3 openssh
 python3 -m ensurepip
 pip3 install --no-cache --upgrade pip setuptools
-pip install paho-mqtt
+pip3 install paho-mqtt
