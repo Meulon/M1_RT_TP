9e7613895fef759056eab5c776992ead98a47bca
meulon <marlon@ik.me>
2022-01-24 16:10:46 +0100

    fix backupserver

--- backupServer.sh
+++ backupServer.sh
@@ -2,6 +2,6 @@
 
 docker build -t backupserver backupServer/
 
-docker run --detach --name some-mariadb --env MARIADB_USER=example-user --env MARIADB_PASSWORD=my_cool_secret --env MARIADB_ROOT_PASSWORD=my-secret-pw backupserver
+docker run --detach --name backupserver --env MARIADB_USER=example-user --env MARIADB_PASSWORD=my_cool_secret --env MARIADB_ROOT_PASSWORD=my-secret-pw backupserver
 
 sudo ovs-docker add-port br eth1 backupserver --ipaddress=192.168.1.6/24 --gateway=192.168.1.1
